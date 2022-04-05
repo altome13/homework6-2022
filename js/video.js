61683f46ebe5b0b12109500dbdcba763751c0e4d
Angelica Tome <37383886+altome13@users.noreply.github.com>
2022-04-05 15:02:40 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,45 @@
+var video = document.querySelector("#player1")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
+	video.autoplay = false;
+	video.loop = false; 
+	// above does not let the video loop
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+// pause video
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+})
+
+// slow down, playbackrate
+document.querySelector("#slower").addEventListener("click", function(){
+	console.log("I want to slow down the video")
+	video.playbackRate *= 0.95
+	console.log("speed is ", video.playbackRate);
+	// line above is like appending the two
+})
+
+document.querySelector("#faster").addEventListener("click", function(){
+	console.log("speeding up");
+	video.playbackRate /= 0.95;
+	console.log("Speed is ", video.playbackRate);
+})
+
+// skip ahead
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("skipping ahead!");
+	video.currentTime += 15;
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0
+	}
+	console.log("Video current time is", video.currentTime)
+});
